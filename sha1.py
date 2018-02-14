173445066bfab2a2d8ca20e04f53cc7bd5eb6d42
Luan Bermudez <35430908+luanber@users.noreply.github.com>
2018-02-14 03:47:42 -0200

    running.... but not right yet

--- sha1.py
+++ sha1.py
@@ -22,7 +22,7 @@ def int2bytes(i):
     return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
 
 
-message = "crypto"
+message = "abc"
 print('original message:', message, '\n')
 #1.1 - converting ASCII to binary:
 #1.2 - add 1 to the end
@@ -55,18 +55,21 @@ print('length of the binary with zeros:', len(binary_0), '\n')
 #now i have to add a 64bit number with the size of the message pre-pad
 
 def add_size(binary_0):
-	print('binary length:', bin(len(binary)), '\n')
+	#print('binary length:', bin(len(binary)), '\n')
 	size = bin(len(binary))[2:]
-	print('size:', size, '\n')
+	#print('size:', size, '\n')
 	#print(size <= ('1' + '0' * 63))
-	if size > '0' : 
+	if int(size,2) < 2**64 : 
 		quotient, reminder = divmod(len(size), 64)
 		#print('quotient:', quotient,'\n', 'reminder:', reminder, '\n')
 		size_batch = '0' * (64 - reminder) + size
-		print('batch size: ', size_batch, '\n')
-		print('size_batch length:', len(size_batch), '\n')
+	#	print('batch size: ', size_batch, '\n')
+	#	print('size_batch length:', len(size_batch), '\n')
 		padded = binary_0 + size_batch
 		return padded
+	else:
+		print("The message is too big .-.")
+
 
 pad = add_size(binary_0)
 
@@ -82,7 +85,7 @@ blocks=[]
 
 for n in range(int(len(pad) / 512)) :
 	blocks.append([])
-	print("block ",n, " size:", len(pad[n * 512 : (n+1) * 512 ]) )
+	print("block ",n, " size:", len(pad[n * 512 : (n+1) * 512 ]), '\n' )
 	for m in range(int(512/32)):
 		blocks[len(blocks)-1].append(pad[(n * 512) + (m * 32) : (n * 512) + (m+1) *32 ])
 
@@ -90,10 +93,11 @@ for n in range(int(len(pad) / 512)) :
 # 16 words each, like this:
 # blocks = [block1, block2, block3= [word1, ....., word 16] ]
 # each word is a "str", with the binary: word2='10010101'
+#print('words\' length is: ', len(blocks[0]), '\n')
 
 
 #SHA-1 constants:
-#hex
+
 def k_int(t):
 	if 0  <= t <= 19:
 		return int('5a827999',16)
@@ -106,9 +110,8 @@ def k_int(t):
 	else:
 		print('something went wrong with \'k\' definition \n')
 
-
 #SHA-1 Initial hash values:
-#hex
+
 H_0_0 = int('67452301',16)
 H_0_1 = int('efcdab89',16)
 H_0_2 = int('98badcfe',16)
@@ -118,7 +121,9 @@ H_0_4 = int('c3d2e1f0',16)
 H = [[H_0_0, H_0_1, H_0_2, H_0_3, H_0_4]];
 
 
-#4 - defining left_rotation:
+#4 - defining rotation:
+
+
 # left_rotation (1010101) = 0101011
 #basically just take the most left digit
 # and put it in the most right place
@@ -126,9 +131,6 @@ H = [[H_0_0, H_0_1, H_0_2, H_0_3, H_0_4]];
 def rotate(lista, n):
 	return lista[-n:] + lista[:-n] #(str)
 
-# a = 65 (num)
-# b = 64 (num)
-# bin(a&b)[2:] is what i was expecting .-.
 # in the blocks lists, we have words='101011....1010', in string form...
 
 def XOR(l1, l2):
@@ -137,8 +139,7 @@ def XOR(l1, l2):
 def AND(l1, l2):
 	return int( bin( l1 & l2 )[2:], 2)
 
-#now the XOR returns a string with a binary number:
-# XOR(int, int ) = '1000'
+# XOR, AND (int, int ) = int
 
 def ft(x,y,z,t):
 	
@@ -151,15 +152,30 @@ def ft(x,y,z,t):
 	
 	if 40 <= t <= 59:
 		return XOR( AND(x,y), XOR(AND(x,z), AND(y,z)) )
+
 #ft retuns "int"
+
+#modular addition
+def ADD(l1, l2, w=32):
+	#in SHA-1 w=32 (bits)
+	quotient, reminder = divmod(l1+l2, 2**w)
+	Z = reminder
+	return Z
+	#what is this doing?
+	#simple: you want to sum numbers lower than 1000
+	# but you don't want to get a sum lower to 1000
+	#if this sum passes 1000, you want just the reminder
+	#so 120+ 430= 650
+	# 550+ 930 = 1480-1000 = 480
+	#this don't give you the right answer, but
+	#mantain a important characterist equal
+	# the reminder....
 
 #5- iteration
 
-print('words\' length is: ', len(blocks[0]), '\n')
-
 for i in range(len(blocks)):
+
 	print("starting iteration...\n")
-	#initializing variables
 
 	for t in range(80):
 
@@ -175,38 +191,56 @@ for i in range(len(blocks)):
 	
 	print('words in the block', i, ' :', len(blocks[i]), '\n')
 
-	a = H[i][0]
-	b = H[i][1]
-	c = H[i][2]
-	d = H[i][3]
-	e = H[i][4]
+	a = H[i][0]  #int
+	b = H[i][1]  #int
+	c = H[i][2]  #int
+	d = H[i][3]  #int
+	e = H[i][4]  #int
 
-	abin = bin(a)[2:]
-	bbin = bin(b)[2:]
+	abin = bin(a)[2:]	#bin
+	bbin = bin(b)[2:]	#bin
 
 	for t in range(80):
-		ft_int = ft(b,c,d,t)
-		arot   = int(rotate(abin, -5), 2)
+		ft_int = ft(b,c,d,t) 		#int
+		arot   = int( rotate(abin, -5), 2) 	#int
 		Tint   = arot + ft_int + e + k_int(t) + int(blocks[i][t],2)
+		quotient, reminder = divmod(Tint, 2**32)
+		T = reminder
 		e = d
 		d = c
 		c = int(rotate(bbin, -30),2)
 		b = a
-		a = Tint
+		a = T
 
 	H.append([])
-	H[i+1].append( a + H[i][0] )
-	H[i+1].append( b + H[i][1] )
-	H[i+1].append( c + H[i][2] )
-	H[i+1].append( d + H[i][3] )
-	H[i+1].append( e + H[i][4] )
+	H[i+1].append( ADD(a, H[i][0]) )
+	H[i+1].append( ADD(b, H[i][1]) )
+	H[i+1].append( ADD(c, H[i][2]) )
+	H[i+1].append( ADD(d, H[i][3]) )
+	H[i+1].append( ADD(e, H[i][4]) )
 
 print(H[len(H)-1])
 
-hash = ''
-for i in range( len(H[len(H)-1]) ):
-	hexad = hex(H[len(H)-1][i])[2:]
-	hash += hexad
 
-print('hash length: ', len(hash))
-print("hash result:", hash , ":)")
+hash_bin = ''
+hash_hex = ''
+for i in range( len(H[len(H)-1]) ):
+	word = bin(H[len(H)-1][i])[2:]
+	quotient, reminder = divmod(len(str(word)), 32)
+	
+	if reminder == 0:
+		word_32 = str(word)
+	else:
+		word_32 = "0" * (32-reminder) + str(word)
+
+	hash_bin += word_32
+	print("the 32bit word", i, "is:      ", word_32)
+	print("the 32bit word", i, "size is: ", len(word_32), '\n')
+
+
+print("hash length: ", len(hash_bin))
+print("hash result: ", hash_bin , ":)")
+
+hash_hex += hex(int(hash_bin,2))[2:]
+print("hash length: ", len(hash_hex))
+print("hash result: ", hash_hex , ":)")
